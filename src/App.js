81fe1c0f092463d2fd31039173006ba011d248b6
Your Name <your.email@example.com>
2025-01-06 15:37:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";  
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./screens/Home";
+
+
+import { BrowserRouter as Router, Routes, Route,link} from "react-router-dom";
+import Login from "./screens/Login";
+
+function App() {
+  return (
+    <Router>
+      <div>
+     <Routes>
+      <Route exact path="/" element={<Home/>} />
+       <Route exact path="/login" element={<Login/>} />
+      </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+ <div> <Navbar/></div>
+<div>Body</div>
+<div><Footer/> </div>
+
+
+
+
+    </div>
+  )
+}
